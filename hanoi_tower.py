3865415369998dd5b22638dba3741754a188d17c
pashkiewich <lordportal922@gmail.com>
2025-02-22 14:17:07 +0200

    add

--- hanoi_tower.py
+++ hanoi_tower.py
@@ -1,1 +1,16 @@
-print(123)
+def move_disk(from_pole, to_pole):
+    print(f"Move disk from {from_pole} to {to_pole}")
+
+
+def hanoi(n, from_pole, to_pole, aux_pole):
+    if n == 1:
+        move_disk(from_pole, to_pole)
+    else:
+        hanoi(n - 1, from_pole, aux_pole, to_pole)
+        move_disk(from_pole, to_pole)
+        hanoi(n - 1, aux_pole, to_pole, from_pole)
+
+
+if __name__ == "__main__":
+    n = 3
+    hanoi(n, "A", "C", "B")
